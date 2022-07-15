b24eb2b922099b2e4b1dde94f914e240335871cb
LucianoSainz <danielsainz28@gmail.com>
2022-07-15 11:20:12 +0200

    finish practice with react todoApp

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,55 +1,17 @@
-import { useEffect, useReducer } from 'react'
-import { todoReducer } from './todoReducer'
 import './App.css'
 import { TodoList } from './components/TodoList'
 import { TodoAdd } from './components/TodoAdd'
+import { useTodos} from './hooks/useTodos';
 
-
-const initialState = [
-  
-]
-   
-const init = () => {
-  return JSON.parse(localStorage.getItem('todos')) || [];
-}
 
 
 export const TodoApp = () => {
 
- const [todos, dispatch] = useReducer(todoReducer, initialState, init)
-
- useEffect(() => {
-      localStorage.setItem('todos', JSON.stringify(todos))
- }, [todos])
-
- const handleNewTodo = (todo) => {
-  const action = {
-    type: '[TODO] Add Todo',
-    payload: todo
-  }
-
-  dispatch(action);
-
- }
-
- const handleDeleteTodo = (id) => {
-  dispatch({
-    type: '[TODO] Remove Todo',
-    payload: id
-  })
- }
-
- const handleToggleTodo = (id) => {
-  dispatch({
-    type: '[TODO] Toggle Todo',
-    payload: id
-
-  })
- }
- 
+  const { todos, todosCount, pendingTodosCount, handleDeleteTodo, handleToggleTodo, handleNewTodo} = useTodos();
+  
   return (
     <>
-    <h1 className='text-center bg-dark bg-gradient p-5'>TodoApp: 10  - <small>pending: 2</small></h1>
+    <h1 className='text-center bg-dark bg-gradient p-5'>{todosCount} - <small>pending: {pendingTodosCount}</small></h1>
     <hr />
     
     <div className='row'>
